814c855200faaa56a37d9a5a3d1beb30849ccf41
unknown <saeed>
2019-05-28 18:43:47 +0300

    adding UpcomingComponent contains new event and new wishes

--- dist/App.f684dadd.js
+++ dist/App.f684dadd.js
@@ -25948,6 +25948,137 @@ function (_React$Component) {
 }(_react.default.Component);
 
 exports.default = MainComponent;
+},{"react":"node_modules/react/index.js"}],"src/UpcomingComponent.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var UpcomingComponent =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(UpcomingComponent, _React$Component);
+
+  function UpcomingComponent() {
+    _classCallCheck(this, UpcomingComponent);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(UpcomingComponent).apply(this, arguments));
+  }
+
+  _createClass(UpcomingComponent, [{
+    key: "render",
+    value: function render() {
+      return _react.default.createElement(_react.default.Fragment, null, _react.default.createElement("h6", {
+        class: "border-bottom border-gray"
+      }, "Latest Upcoming Events"), _react.default.createElement("div", {
+        class: "media text-muted pt-3"
+      }, _react.default.createElement("svg", {
+        class: "bd-placeholder-img mr-2 rounded",
+        width: "32",
+        height: "32",
+        xmlns: "http://www.w3.org/2000/svg",
+        preserveAspectRatio: "xMidYMid slice",
+        focusable: "false",
+        role: "img",
+        "aria-label": "Placeholder: 32x32"
+      }, _react.default.createElement("title", null, "Placeholder"), _react.default.createElement("rect", {
+        width: "100%",
+        height: "100%",
+        fill: "#007bff"
+      }), _react.default.createElement("text", {
+        x: "50%",
+        y: "50%",
+        fill: "#007bff",
+        dy: ".3em"
+      }, "32x32")), _react.default.createElement("p", {
+        class: "media-body pb-3 mb-0 small lh-125 border-bottom border-gray"
+      }, _react.default.createElement("strong", {
+        class: "d-block text-gray-dark"
+      }, "@username"), "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.")), _react.default.createElement("div", {
+        class: "media text-muted pt-3"
+      }, _react.default.createElement("svg", {
+        class: "bd-placeholder-img mr-2 rounded",
+        width: "32",
+        height: "32",
+        xmlns: "http://www.w3.org/2000/svg",
+        preserveAspectRatio: "xMidYMid slice",
+        focusable: "false",
+        role: "img",
+        "aria-label": "Placeholder: 32x32"
+      }, _react.default.createElement("title", null, "Placeholder"), _react.default.createElement("rect", {
+        width: "100%",
+        height: "100%",
+        fill: "#e83e8c"
+      }), _react.default.createElement("text", {
+        x: "50%",
+        y: "50%",
+        fill: "#e83e8c",
+        dy: ".3em"
+      }, "32x32")), _react.default.createElement("p", {
+        class: "media-body pb-3 mb-0 small lh-125 border-bottom border-gray"
+      }, _react.default.createElement("strong", {
+        class: "d-block text-gray-dark"
+      }, "@username"), "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.")), _react.default.createElement("div", {
+        class: "media text-muted pt-3"
+      }, _react.default.createElement("svg", {
+        class: "bd-placeholder-img mr-2 rounded",
+        width: "32",
+        height: "32",
+        xmlns: "http://www.w3.org/2000/svg",
+        preserveAspectRatio: "xMidYMid slice",
+        focusable: "false",
+        role: "img",
+        "aria-label": "Placeholder: 32x32"
+      }, _react.default.createElement("title", null, "Placeholder"), _react.default.createElement("rect", {
+        width: "100%",
+        height: "100%",
+        fill: "#6f42c1"
+      }), _react.default.createElement("text", {
+        x: "50%",
+        y: "50%",
+        fill: "#6f42c1",
+        dy: ".3em"
+      }, "32x32")), _react.default.createElement("p", {
+        class: "media-body pb-3 mb-0 small lh-125 border-bottom border-gray"
+      }, _react.default.createElement("strong", {
+        class: "d-block text-gray-dark"
+      }, "@username"), "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.")), _react.default.createElement("small", {
+        class: "d-block text-right mt-3"
+      }, _react.default.createElement("a", {
+        href: "#"
+      }, "All updates")));
+    }
+  }]);
+
+  return UpcomingComponent;
+}(_react.default.Component);
+
+var _default = UpcomingComponent;
+exports.default = _default;
 },{"react":"node_modules/react/index.js"}],"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
@@ -26031,13 +26162,21 @@ var _NavBarCoponent = _interopRequireDefault(require("./NavBarCoponent"));
 
 var _MainComponent = _interopRequireDefault(require("./MainComponent"));
 
+var _UpcomingComponent = _interopRequireDefault(require("./UpcomingComponent"));
+
 require("bootstrap/dist/css/bootstrap.css");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 //browser
 _reactDom.default.render(_react.default.createElement(_NavBarCoponent.default, null), document.querySelector('#navBar'));
-},{"react-dom":"node_modules/react-dom/index.js","react":"node_modules/react/index.js","./NavBarCoponent":"src/NavBarCoponent.js","./MainComponent":"src/MainComponent.js","bootstrap/dist/css/bootstrap.css":"node_modules/bootstrap/dist/css/bootstrap.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+_reactDom.default.render(_react.default.createElement(_UpcomingComponent.default, null), document.querySelector('#leftSide'));
+
+_reactDom.default.render(_react.default.createElement(_UpcomingComponent.default, null), document.querySelector('#rightSide'));
+
+_reactDom.default.render(_react.default.createElement(_UpcomingComponent.default, null), document.querySelector('#content'));
+},{"react-dom":"node_modules/react-dom/index.js","react":"node_modules/react/index.js","./NavBarCoponent":"src/NavBarCoponent.js","./MainComponent":"src/MainComponent.js","./UpcomingComponent":"src/UpcomingComponent.js","bootstrap/dist/css/bootstrap.css":"node_modules/bootstrap/dist/css/bootstrap.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -26065,7 +26204,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50925" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53331" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@
     import React from 'react';
      import NavBarCoponent from './NavBarCoponent';
      import MainComponent from './MainComponent';
+     import UpcomingComponent from './UpcomingComponent'
      import 'bootstrap/dist/css/bootstrap.css';
 
      ReactDOM.render(<NavBarCoponent />,
          document.querySelector('#navBar'));
+
+         ReactDOM.render(<UpcomingComponent />,
+            document.querySelector('#leftSide'));
+
+            ReactDOM.render(<UpcomingComponent />,
+                document.querySelector('#rightSide'));
+
+                ReactDOM.render(<UpcomingComponent />,
+                    document.querySelector('#content'));
+
         
